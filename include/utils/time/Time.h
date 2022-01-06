065ff20d30af104c910eee5983bf49216e5f142b
zhivko.petrov <zhivko.petrov@ocado.com>
2022-01-06 10:24:02 +0200

    chore: swapped OS depentant implementation with standard one

--- include/utils/time/Time.h
+++ include/utils/time/Time.h
@@ -5,143 +5,25 @@
 
 // C++ system headers
 #include <cstdint>
-#include <ctime>
+#include <chrono>
 
 // Other libraries headers
 
 // Own components headers
-#include "utils/time/TimerDefines.h"
 
 class Time {
- public:
-  Time();
-
-  /** @brief used to measure elapsed time since last invoke of this method
-   *         NOTE: .getElapsed() method simply measures time between 2
-   *         points of time. In order to display this time chain the
-   *         function with one of the below display functions.
-   *         Example:
-   *         Time time;
-   *
-   *         //something you want to measure
-   *
-   *         const int64_t elapsedTime =
-   *                                   time.getElapsed().toMilliseconds();
-   *
-   *  @returns Time & - *this instance
-   * */
+public:
   Time& getElapsed();
 
-  /** @brief used to measure elapsed time since the creation of
-   *                                                   the Time instance.
-   *         NOTE: In order to display this time chain the
-   *         function with one of the below display functions.
-   *         Example:
-   *         Time time;
-   *
-   *         //something you want to measure
-   *
-   *         const int64_t elapsedTime =
-   *                               time.getElapsedFromStart().toSeconds();
-   *
-   *  @returns Time & - *this instance
-   * */
-  Time& getElapsedFromStart();
+  int64_t toSeconds() const;
+  int64_t toMilliseconds() const;
+  int64_t toMicroseconds() const;
+  int64_t toNanoseconds() const;
 
-  /** @brief used to display the elapsed time between two calls of the
-   *         .getElapsed() method in nanoseconds
-   *
-   *  @returns int64_t - elapsed time in nanoseconds
-   * */
-  int64_t toNanoseconds() const {
-    return _elapsed.tv_sec * NANOSECOND + _elapsed.tv_nsec;
-  }
-
-  /** @brief used to display the elapsed time between two calls of the
-   *         .getElapsed() method in microseconds
-   *
-   *  @returns int64_t - elapsed time in microseconds
-   * */
-  int64_t toMicroseconds() const {
-    return _elapsed.tv_sec * MILLISECOND + _elapsed.tv_nsec / MICROSECOND;
-  }
-
-  /** @brief used to display the elapsed time between two calls of the
-   *         .getElapsed() method in milliseconds
-   *
-   *  @returns int64_t - elapsed time in milliseconds
-   * */
-  int64_t toMilliseconds() const {
-    return _elapsed.tv_sec * MICROSECOND + _elapsed.tv_nsec / MILLISECOND;
-  }
-
-  /** @brief used to display the elapsed time between two calls of the
-   *         .getElapsed() method in seconds
-   *
-   *  @returns int64_t - elapsed time in seconds
-   * */
-  int64_t toSeconds() const { return _elapsed.tv_sec; }
-
-  /** @brief used to acquiare system clock time with
-   *                                          getClockTime kernel invoke
-   *
-   *  @param timespec * - populated timespec struct
-   * */
-  static void getClockTime(timespec* outClockTime);
-
-  /** @brief used to get unique timestamp (seconds precision)
-   *
-   *  @returns int64_t - unique timestamp (in seconds)
-   * */
-  static inline int64_t getTimestamp() { return time(0); }
-
-  /** @brief used to display the value of input timespec in nanoseconds
-   *
-   *  @param const timespec & - timespec struct to be converted
-   *
-   *  @returns int64_t        - value of input timespec in nanoseconds
-   * */
-  static inline int64_t toNanoseconds(const timespec& ts) {
-    return ts.tv_sec * NANOSECOND + ts.tv_nsec;
-  }
-
-  /** @brief used to display the value of input timespec in microseconds
-   *
-   *  @param const timespec & - timespec struct to be converted
-   *
-   *  @returns int64_t        - value of input timespec in microseconds
-   * */
-  static inline int64_t toMicroseconds(const timespec& ts) {
-    return ts.tv_sec * MILLISECOND + ts.tv_nsec / MICROSECOND;
-  }
-
-  /** @brief used to display the value of input timespec in milliseconds
-   *
-   *  @param const timespec & - timespec struct to be converted
-   *
-   *  @returns int64_t        - value of input timespec in milliseconds
-   * */
-  static inline int64_t toMilliseconds(const timespec& ts) {
-    return ts.tv_sec * MICROSECOND + ts.tv_nsec / MILLISECOND;
-  }
-
-  /** @brief used to display the value of input timespec in seconds
-   *
-   *  @param const timespec & - timespec struct to be converted
-   *
-   *  @returns int64_t        - value of input timespec in seconds
-   * */
-  static inline int64_t toSeconds(const timespec& ts) { return ts.tv_sec; }
-
- private:
-  // used to measure time from the creation of the Time instance
-  timespec _startTime;
-
-  // used to measure elapsed time since last getElapsed() call
-  timespec _elapsed;
-
-  // used to help calculate the elapsed time since last getElapsed() call
-  timespec _elapsedLastCall;
+private:
+  std::chrono::time_point<std::chrono::steady_clock> _now =
+      std::chrono::steady_clock::now();
+  std::chrono::time_point<std::chrono::steady_clock> _elapsedLastCall;
 };
 
 #endif /* UTILS_TIME_H_ */
--- src/time/Time.cpp
+++ src/time/Time.cpp
@@ -4,67 +4,34 @@
 // C system headers
 
 // C++ system headers
-#include <cerrno>
-#include <cstring>
 
 // Other libraries headers
 
 // Own components headers
-#include "utils/ErrorCode.h"
-#include "utils/Log.h"
-
-Time::Time() {
-  if (SUCCESS != clock_gettime(CLOCK_MONOTONIC, &_startTime)) {
-    LOGERR("clock_gettime() failed, reason: %s", strerror(errno));
-  }
-
-  _elapsedLastCall.tv_sec = _startTime.tv_sec;
-  _elapsedLastCall.tv_nsec = _startTime.tv_nsec;
-  _elapsed.tv_sec = 0;
-  _elapsed.tv_nsec = 0;
-}
 
 Time& Time::getElapsed() {
-  timespec now;
-
-  if (SUCCESS != clock_gettime(CLOCK_MONOTONIC, &now)) {
-    LOGERR("clock_gettime() failed, reason: %s", strerror(errno));
-  }
-
-  _elapsed.tv_sec = now.tv_sec - _elapsedLastCall.tv_sec;
-  _elapsed.tv_nsec = now.tv_nsec - _elapsedLastCall.tv_nsec;
-
-  _elapsedLastCall.tv_sec = now.tv_sec;
-  _elapsedLastCall.tv_nsec = now.tv_nsec;
-
-  if (0 > _elapsed.tv_nsec) {
-    _elapsed.tv_nsec += SECOND;
-    --_elapsed.tv_sec;
-  }
+  _elapsedLastCall = _now;
+  _now = std::chrono::steady_clock::now();
 
   return *this;
 }
 
-Time& Time::getElapsedFromStart() {
-  timespec now;
-
-  if (SUCCESS != clock_gettime(CLOCK_MONOTONIC, &now)) {
-    LOGERR("clock_gettime() failed, reason: %s", strerror(errno));
-  }
-
-  _elapsed.tv_sec = now.tv_sec - _startTime.tv_sec;
-  _elapsed.tv_nsec = now.tv_nsec - _startTime.tv_nsec;
-
-  if (0 > _elapsed.tv_nsec) {
-    _elapsed.tv_nsec += SECOND;
-    --_elapsed.tv_sec;
-  }
-
-  return *this;
+int64_t Time::toSeconds() const {
+  return std::chrono::duration_cast<std::chrono::seconds>(
+      _now - _elapsedLastCall).count();
 }
 
-void Time::getClockTime(timespec* outClockTime) {
-  if (SUCCESS != clock_gettime(CLOCK_REALTIME, outClockTime)) {
-    LOGERR("clock_gettime() failed, reason: %s", strerror(errno));
-  }
+int64_t Time::toMilliseconds() const {
+  return std::chrono::duration_cast<std::chrono::milliseconds>(
+      _now - _elapsedLastCall).count();
+}
+
+int64_t Time::toMicroseconds() const {
+  return std::chrono::duration_cast<std::chrono::microseconds>(
+      _now - _elapsedLastCall).count();
+}
+
+int64_t Time::toNanoseconds() const {
+  return std::chrono::duration_cast<std::chrono::nanoseconds>(
+      _now - _elapsedLastCall).count();
 }
