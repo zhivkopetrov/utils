a727bf862182167e2889a7df39e38d2b6b10537f
zhivko.petrov <zhivko.petrov@ocado.com>
2022-01-12 11:52:16 +0200

    feat: add suport for install_and_export_target and enable_ros_tooling_for_target

---
+++ utilsConfig.cmake
@@ -0,0 +1,10 @@
+include(CMakeFindDependencyMacro)
+
+if(NOT TARGET utils::utils)
+  include(${CMAKE_CURRENT_LIST_DIR}/utilsTargets.cmake)
+endif()
+
+# This is for catkin compatibility.
+set(utils_LIBRARIES utils::utils)
+get_target_property(utils_INCLUDE_DIRS utils::utils INTERFACE_INCLUDE_DIRECTORIES)
+
