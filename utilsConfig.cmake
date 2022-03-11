99104aa500a19c03474bb32f9ab28e1cc441fc27
zhivko.petrov <zhivko.petrov@ocado.com>
2022-03-11 12:25:54 +0200

    fix: add forgotten dependency for find_package on utils library

--- utilsConfig.cmake
+++ utilsConfig.cmake
@@ -1,5 +1,9 @@
 include(CMakeFindDependencyMacro)
 
+#find_dependency will correctly forward REQUIRED or QUIET args to the consumer
+#find_package is only for internal use
+find_dependency(Threads REQUIRED)
+
 if(NOT TARGET utils::utils)
   include(${CMAKE_CURRENT_LIST_DIR}/utilsTargets.cmake)
 endif()
