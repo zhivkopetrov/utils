2973e59d288161bc5aea599fc97cf7d2f6dab6a2
Zhivko Petrov <zhivko.petrov@ocado.com>
2022-04-04 15:17:09 +0300

    fix: Ament compatibility

--- utilsConfig.cmake
+++ utilsConfig.cmake
@@ -2,8 +2,8 @@ include(CMakeFindDependencyMacro)
 
 #find_dependency will correctly forward REQUIRED or QUIET args to the consumer
 #find_package is only for internal use
-find_dependency(Threads REQUIRED)
 find_dependency(cmake_helpers REQUIRED)
+find_dependency(Threads REQUIRED)
 
 if(NOT TARGET utils::utils)
   include(${CMAKE_CURRENT_LIST_DIR}/utilsTargets.cmake)
