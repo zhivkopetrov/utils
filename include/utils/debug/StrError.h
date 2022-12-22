7ac6b476dbb5c54bbb4770cda2cc57ac1e84fc20
Zhivko Petrov <zhivko.petrov@ocado.com>
2022-12-22 16:54:15 +0200

    feat: impleement cross-platform strerror() method. Still thread-unsafe, though

---
+++ include/utils/debug/StrError.h
@@ -0,0 +1,14 @@
+// System headers
+#include <string>
+
+// Other libraries headers
+
+// Own components headers
+
+// Forward declarations
+
+/* Warning: 
+ *    this method is not thread safe, because an 
+ *    internal static buffer is being used
+ * */
+std::string strError();
---
+++ src/debug/StrError.cpp
@@ -0,0 +1,20 @@
+#if defined(_WIN32) || defined(_WIN64)
+  // disable deprecation warnings on windows 
+  #define _CRT_SECURE_NO_WARNINGS
+#endif
+
+// Corresponding header
+#include "utils/debug/StrError.h"
+
+// System headers
+#include <cstring>
+#include <cerrno>
+
+// Other libraries headers
+
+// Own components headers
+#include "utils/Log.h"
+
+std::string strError() {
+  return strerror(errno);
+}
